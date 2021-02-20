4bc757491076f8bdc514667f444a386087359502
EnzoAlexsander <enzoalexsander9@gmail.com>
2021-02-20 20:00:29 -0300

    [UPDATE] Atualizando o server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.send('ok')
-})
+const passengers = [
+    {
+        name: 'Enzo',
+        flightNumber: 7859,
+        time: '18h00',
+    },
+    {
+        name: 'Alessandra',
+        flightNumber: 7859,
+        time: '18h00',
+    },
+    {
+        name: 'Fred',
+        flightNumber: 7859,
+        time: '18h00',
+    },
+    {
+        name: 'Rambo',
+        flightNumber: 7859,
+        time: '18h00',
+    },
+];
 
+app.get('/', (request, response) => {
+    const filePath = path.join(__dirname, 'print.ejs');
+    ejs.renderFile(filePath, { passengers });
+
+    return response.send(passengers);
+});
 
 app.listen(3000);

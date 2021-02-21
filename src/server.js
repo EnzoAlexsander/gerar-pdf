37f63048007001f14e53e7d6e3ca582360b723ef
EnzoAlexsander <enzoalexsander9@gmail.com>
2021-02-21 15:20:53 -0300

    [UPDATE] Implementando atualizações

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const pdf = require('html-pdf');
 const app = express();
 
 const passengers = [
@@ -28,9 +29,32 @@ const passengers = [
 
 app.get('/', (request, response) => {
     const filePath = path.join(__dirname, 'print.ejs');
-    ejs.renderFile(filePath, { passengers });
+    ejs.renderFile(filePath, { passengers }, (error, html) => {
+        if (error) {
+            return response.send('Erro na leitura do arquivo');
+        }
 
-    return response.send(passengers);
+        const options = {
+            height: '11.25in',
+            width: '8.5in',
+            header: {
+                height: '20mm',
+            },
+            footer: {
+                height: '20mm',
+            },
+        };
+
+        //criar o pdf
+        pdf.create(html, options).toFile('report.pdf', (error, data) => {
+            if(error) {
+                return response.send('Erro ao gerar o PDF')
+            }
+
+            // eviar para o navegador
+            return response.send(html);
+        })
+    });
 });
 
 app.listen(3000);
